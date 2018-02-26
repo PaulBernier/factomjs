c967fc8550c75564444248d65dfdbc4442aba8f1
Paul <bernier.pja@gmail.com>
2018-02-25 18:21:22 -0800

    First test

---
+++ test/entry.spec.js
@@ -0,0 +1,26 @@
+const assert = require('chai').assert,
+    {
+        Entry,
+        entryCost,
+        entrySize
+    } = require('../src/entry');
+
+describe('Test Entry', function () {
+
+    it('should populate Entry attributes', function () {
+        const entry = new Entry.Builder()
+            .chainId('cfb5d93e747d20433e3b14603f90a5eb152d0399e7278f9671ecf9763f8780e8', 'hex')
+            .extId('extId')
+            .extId(Buffer.from('extId2'))
+            .content('hello')
+            .build();
+
+        assert.instanceOf(entry.chainId, Buffer);
+        assert.instanceOf(entry.extIds, Array);
+        assert.instanceOf(entry.content, Buffer);
+        assert.lengthOf(entry.extIds, 2);
+        assert.lengthOf(entry.extIds[0], 5);
+        assert.lengthOf(entry.extIds[1], 6);
+    });
+
+});
