10874366776a8d820a0a1ef8ce1c4e054ead77b0
Paul <bernier.pja@gmail.com>
2018-05-18 16:51:58 -0700

    Fix gulp and add minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 const gulp = require('gulp'),
     browserify = require('browserify'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
     buffer = require('vinyl-buffer'),
     source = require('vinyl-source-stream');
 
 gulp.task('browserify', function() {
     return browserify({ entries: ['src/factom.js'] })
-        .transform('babelify', { presets: ['es2015'] })
+        .transform('babelify', { presets: ['es2015'], plugins: ['transform-async-to-generator'] })
         .bundle()
         .pipe(source('factom.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename({ extname: '.min.js' }))
         .pipe(buffer())
+        .pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['browserify']);
+gulp.task('default', gulp.parallel(['browserify']));
