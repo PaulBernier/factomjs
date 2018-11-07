e8319b8861dde9f0742cfc60a9d27389559a9e54
Paul Bernier <bernier.pja@gmail.com>
2018-11-07 18:25:14 +1100

    Add JSDoc to entry file

--- src/entry.js
+++ src/entry.js
@@ -3,11 +3,26 @@ const sign = require('tweetnacl/nacl-fast').sign,
     { MAX_ENTRY_PAYLOAD_SIZE } = require('./constant'),
     { sha256, sha512 } = require('./util');
 
-
 /**********************
  * Entry class
  **********************/
 
+/**
+ * Class representing an Entry
+ * @param {EntryBuilder} builder 
+ * @property {Buffer} chainId - Chain ID.
+ * @property {Buffer[]} extIds - External IDs.
+ * @property {Buffer} content - Content.
+ * @property {number} timestamp - Timestamp in milliseconds for the commit.
+ * @property {EntryBlockContext} blockContext - Block context. This property is *not* populated when using the method getEntry.
+ * @example
+    const myEntry = Entry.builder()
+        .chainId('9107a308f91fd7962fecb321fdadeb37e2ca7d456f1d99d24280136c0afd55f2')
+        .extId('6d79206578742069642031') // If no encoding parameter is passed as 2nd argument, 'hex' is used as default
+        .extId('Some external ID', 'utf8')
+        .content('My new content',  'utf8')
+        .build();
+ */
 class Entry {
     constructor(builder) {
         if (builder instanceof EntryBuilder) {
@@ -22,31 +37,57 @@ class Entry {
         }
     }
 
-    get contentHex() {
-        return this.content.toString('hex');
-    }
-
-    get extIdsHex() {
-        return this.extIds.map(extId => extId.toString('hex'));
-    }
-
+    /**
+     * @returns {string} Chain ID of the entry as hex encoded string.
+     */
     get chainIdHex() {
         return this.chainId.toString('hex');
     }
 
+    /**
+     * @returns {string} Entry content as hex encoded string.
+     */
+    get contentHex() {
+        return this.content.toString('hex');
+    }
+
+    /**
+     * @returns {string[]} External ids as hex encoded strings.
+     */
+    get extIdsHex() {
+        return this.extIds.map(extId => extId.toString('hex'));
+    }
+
+
+    /**
+     * Get the entry size.
+     * @returns {number} The entry size in bytes.
+     */
     size() {
         // Header size is 35 for the first 127 versions
         return 35 + this.payloadSize();
     }
 
+    /**
+     * Get the entry payload size (excluding the header).
+     * @returns {number} The entry payload size in bytes.
+     */
     payloadSize() {
         return this.rawDataSize() + 2 * this.extIds.length;
     }
 
+    /**
+     * Get the entry raw data size (payload size excluding the 2 byte overhead per extID).
+     * @returns {number} The entry raw size in bytes.
+     */
     rawDataSize() {
         return this.content.length + this.extIds.reduce((acc, value) => acc + value.length, 0);
     }
 
+    /**
+     * Get the number of bytes that can be added to the entry for the same EC cost.
+     * @returns {number} Remaining number of free bytes.
+     */
     remainingFreeBytes() {
         const size = this.payloadSize();
         if (size === 0) {
@@ -56,6 +97,10 @@ class Entry {
         return remainder ? 1024 - remainder : 0;
     }
 
+    /**
+     * Get the number of bytes that can be added to the entry before hitting the maximum (10kb).
+     * @returns {number} Maximum number of bytes that can still be added to the entry. 
+     */
     remainingMaxBytes() {
         const remainingMaxBytes = MAX_ENTRY_PAYLOAD_SIZE - this.payloadSize();
         if (remainingMaxBytes < 0) {
@@ -65,15 +110,26 @@ class Entry {
         return remainingMaxBytes;
     }
 
+    /**
+     * Get hash of the entry.
+     * @returns {Buffer} Hash of the entry. 
+     */
     hash() {
         const data = this.marshalBinary();
         return sha256(Buffer.concat([sha512(data), data]));
     }
 
+    /**
+     * @returns {string} Hash of the entry as hex encoded string.
+     */
     hashHex() {
         return this.hash().toString('hex');
     }
 
+
+    /**
+     * @returns {Buffer} Result of marshaling the entry.
+     */
     marshalBinary() {
         if (this.chainId.length === 0) {
             throw new Error('ChainId is missing to marshal the entry');
@@ -84,10 +140,17 @@ class Entry {
         return Buffer.concat([header, externalIds, this.content]);
     }
 
+    /**
+     * @returns {Buffer} Result of marshaling the entry as hex encoded string.
+     */
     marshalBinaryHex() {
         return this.marshalBinary().toString('hex');
     }
 
+    /**
+     * Get Entry Credit cost of the entry.
+     * @returns {number} EC cost of the entry. 
+     */
     ecCost() {
         const dataLength = this.payloadSize();
         if (dataLength > MAX_ENTRY_PAYLOAD_SIZE) {
@@ -97,6 +160,11 @@ class Entry {
         return Math.ceil(dataLength / 1024);
     }
 
+    /**
+     * Entry builder static factory.
+     * @param {Entry} [entry] - Optional entry to use to initialize the attributes of the builder.
+     * @returns {EntryBuilder} A new EntryBuilder.
+     */
     static builder(entry) {
         return new EntryBuilder(entry);
     }
@@ -106,7 +174,12 @@ class Entry {
  * Entry builder class
  **********************/
 
+/**
+ * Class to build an {@link Entry}
+ * @param {Entry} [entry] - Optional entry to use to initialize the attributes of the builder.
+ */
 class EntryBuilder {
+
     constructor(entry) {
         if (entry instanceof Object) {
             this._chainId = entry.chainId ? Buffer.from(entry.chainId, 'hex') : Buffer.from('');
@@ -119,42 +192,93 @@ class EntryBuilder {
             this._chainId = Buffer.from('');
         }
     }
+    /**
+     * Set content.
+     * @param {string|Buffer} content | Content.
+     * @param {string} [enc=hex] - Encoding of the content if it is a string.
+     * @returns {EntryBuilder} - EntryBuilder instance.
+     */
     content(content, enc) {
         if (content) {
             this._content = Buffer.from(content, enc || 'hex');
         }
         return this;
     }
+    /**
+     * Set chain ID.
+     * @param {string|Buffer} chainId - Chain ID.
+     * @param {string} [enc=hex] - Encoding of the chainId if it is a string.
+     * @returns {EntryBuilder} - EntryBuilder instance.
+     */
     chainId(chainId, enc) {
         if (chainId) {
             this._chainId = Buffer.from(chainId, enc || 'hex');
         }
         return this;
     }
+    /**
+     * Set external IDs.
+     * @param {string[]|Buffer[]} extIds - External IDs.
+     * @param {string} [enc=hex] - Encoding of the external ids if they are strings.
+     * @returns {EntryBuilder} - EntryBuilder instance.
+     */
     extIds(extIds, enc) {
         if (Array.isArray(extIds)) {
             this._extIds = extIds.map(extId => Buffer.from(extId, enc || 'hex'));
         }
         return this;
     }
+    /**
+     * Add an external ID.
+     * @param {string|Buffer} extId - External ID.
+     * @param {string} [enc=hex] - Encoding of the external id if it is a string.
+     * @returns {EntryBuilder} - EntryBuilder instance.
+     */
     extId(extId, enc) {
         if (extId) {
             this._extIds.push(Buffer.from(extId, enc || 'hex'));
         }
         return this;
     }
+    /**
+     * Set explicit timestamp for the entry commit. 
+     * If not set the library will use Date.now() as the commit timestamp.
+     * @param {number} timestamp - Timestamp in milliseconds.
+     * @returns {EntryBuilder} - EntryBuilder instance.
+     */
     timestamp(timestamp) {
         this._timestamp = timestamp;
         return this;
     }
+
+    /**
+     * Set block context. This method is used internally by the library to populate a block context, 
+     * regular users should not have to use this.
+     * @param {EntryBlockContext} blockContext 
+     * @returns {EntryBuilder} - EntryBuilder instance.
+     */
     blockContext(blockContext) {
         this._blockContext = blockContext;
         return this;
     }
+
+    /**
+     * @returns {Entry} - Built entry.
+     */
     build() {
         return new Entry(this);
     }
 }
+
+/**
+ * Block context of an {@link Entry}.
+ * @typedef {Object} EntryBlockContext
+ * @property {number} entryTimestamp - Epoch timestamp (in seconds) of the entry.
+ * @property {number} directoryBlockHeight - Directory Block height.
+ * @property {number} entryBlockTimestamp - Epoch timestamp (in seconds) of the Entry Block.
+ * @property {number} entryBlockSequenceNumber - Entry Block sequence number.
+ * @property {string} entryBlockKeyMR - Entry Block KeyMR.
+ */
 
 /**********************
  * Marshal and compose
@@ -182,6 +306,14 @@ function marshalExternalIdsBinary(extIds) {
     return Buffer.concat(result);
 }
 
+/**
+ * Compose the commit of an Entry, that can then be used as input of the factomd API `commit-entry`.
+ * Note that if the Entry doen't have a timestamp set the library will use Date.now() as the default for the commit timestamp.
+ * @param {Entry} entry - Entry to compose the commit of.
+ * @param {string} ecAddress - Entry Credit address that pays for the commit, either private (Es) or public (EC). If a public EC address is provided it is necessary to manually pass the signature of the commit as a 3rd argument (use case for hardware wallets)
+ * @param {string|Buffer} [sig] - Optional signature of the commit (composeEntryLedger). Only necessary if a public EC address was passed as 2nd argument.
+ * @returns {Buffer} - Entry commit.
+ */
 function composeEntryCommit(entry, ecAddress, sig) {
     validateEntryInstance(entry);
 
@@ -225,11 +357,23 @@ function composeEntryLedger(entry) {
     return buffer;
 }
 
+/**
+ * Compose the reveal of an Entry, that can then be used as input of the factomd API `reveal-entry`.
+ * @param {Entry} entry - Entry to compose the reveal of.
+ * @returns {Buffer} - Entry reveal.
+ */
 function composeEntryReveal(entry) {
     validateEntryInstance(entry);
     return entry.marshalBinary();
 }
 
+/**
+ * Compose the commit and reveal of an Entry, that can then be used as inputs of the factomd APIs `commit-entry` and `reveal-entry`.
+ * @param {Entry} entry - Entry to compose the commit and reveal of.
+ * @param {string} ecAddress - Entry Credit address that pays for the commit, either private (Es) or public (EC). If a public EC address is provided it is necessary to manually pass the signature of the commit as a 3rd argument (use case for hardware wallets)
+ * @param {string|Buffer} [signature] - Optional signature of the commit (composeEntryLedger). Only necessary if a public EC address was passed as 2nd argument.
+ * @returns {{commit:Buffer, reveal:Buffer}} - Entry commit and reveal.
+ */
 function composeEntry(entry, ecAddress, signature) {
     validateEntryInstance(entry);
 
@@ -249,6 +393,12 @@ function validateEntryInstance(entry) {
     }
 }
 
+/**
+ * Compute the transaxtion id of the Entry commit. The transaction ID is dependent on the timestamp set in the entry.
+ * If the timestamp is not set the library uses Date.now() as the default, changing the result of this function if called at different times. 
+ * @param {Entry} entry 
+ * @returns {Buffer} - The transaction id of the Entry commit.
+ */
 function computeEntryTxId(entry) {
     validateEntryInstance(entry);
     return sha256(composeEntryLedger(entry));
